ee8761f327b97c073bae88d012b521d094b31844
MuhammadHaris55 <hkhn567@gmail.com>
2023-03-24 15:44:58 +0500

    version-5 uploaded and published on play store on 24 March-23

--- lib/constants/utils.dart
+++ lib/constants/utils.dart
@@ -3,7 +3,8 @@ import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:open_file/open_file.dart';
+// import 'package:open_file/open_file.dart';
+import 'package:open_filex/open_filex.dart';
 import 'package:path_provider/path_provider.dart';
 
 void showSnackBar(BuildContext context, String text) {
@@ -23,7 +24,8 @@ Future openFile(BuildContext context, String imagePath, String fileName) async {
     return;
   }
   // print('Path: ${file.path}');
-  OpenFile.open(file.path);
+  // OpenFile.open(file.path);
+  OpenFilex.open(file.path);
 }
 
 Future<File?> downloadFile(
